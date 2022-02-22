9f11807a309f31f3e676a9266c9945d8575c1703
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-02-22 11:54:42 -0300

    crud read

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -2,20 +2,25 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import com.mysql.cj.protocol.Resultset;
 
 public class DAO {
-		//Módulo de conexão
-		// Parâmetros de conexão
-	
+	// Módulo de conexão
+	// Parâmetros de conexão
+
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
 	private String user = "root";
 	private String password = "mysql";
-	
-	//Método de conexão
+
+	// Método de conexão
 	private Connection conectar() {
 		Connection con = null;
-		
+
 		try {
 			Class.forName(driver);
 			con = DriverManager.getConnection(url, user, password);
@@ -25,17 +30,59 @@ public class DAO {
 			return null;
 		}
 	}
-	
-	/*
-	 teste de conexão
-	public void testeConexao() {
+	// CRUD CREATE
+
+	public void inserirContato(JavaBeans contato) {
+		String create = "INSERT INTO contatos(nome,fone,email) values (?,?,?)";
 		try {
+			// abrir a conexão com o DB
 			Connection con = conectar();
-			System.out.println(con);
+
+			// Preparar a query para execução no DB
+			PreparedStatement pst = con.prepareStatement(create);
+
+			// Substituir os parâmetros (?) pelo conteúdo das variáveis JavaBeans
+			pst.setString(1, contato.getNome());
+			pst.setString(2, contato.getFone());
+			pst.setString(3, contato.getEmail());
+
+			// executar a query
+			pst.executeUpdate();
+
+			// Encerrar a conexão com o DB
 			con.close();
+
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	*/
+
+	// CRUD READ
+	public ArrayList<JavaBeans> listarContatos() {
+		// Criando um objeto para acessar a classe Javabeans
+		ArrayList<JavaBeans> contatos = new ArrayList<>();
+
+		String read = "SELECT * FROM  contatos ORDER BY nome";
+		try {
+			Connection con = conectar();
+			PreparedStatement pst = con.prepareStatement(read);
+			ResultSet rs = pst.executeQuery();
+
+			// O laço abaixo será executado equanto houver contatos
+			while (rs.next()) {
+				String idcon = rs.getString(1);
+				String nome = rs.getString(2);
+				String fone = rs.getString(3);
+				String email = rs.getString(4);
+
+				// populando o Arraylist
+				contatos.add(new JavaBeans(idcon, nome, fone, email));
+			}
+			con.close();
+			return contatos;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
 }
