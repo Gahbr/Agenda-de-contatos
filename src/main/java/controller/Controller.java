cf5cdab1b70d00298d2ec5b1e4ad409b4d2a679b
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-02-24 11:51:49 -0300

    crud update

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,18 +2,16 @@ package controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
-
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import model.DAO;
 import model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert", "/select", "/update" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -36,7 +34,11 @@ public class Controller extends HttpServlet {
 			contatos(request, response);
 		} else if (action.equals("/insert")) {
 			novoContato(request, response);
-		} else {
+		} else if (action.equals("/select")) {
+			listarContato(request, response);
+		}  else if (action.equals("/update")) {
+			editarContato(request, response);
+		}else {
 			response.sendRedirect("index.html");
 		}
 
@@ -48,10 +50,16 @@ public class Controller extends HttpServlet {
 
 		// Criando um objeto que irá receber os dados JavaBeans
 		ArrayList<JavaBeans> lista = dao.listarContatos();
-		
+
+		//
+
+		// Encaminhar a lista ao documento agenda.jsp
+		request.setAttribute("contatos", lista);
+		RequestDispatcher rd = request.getRequestDispatcher("agenda.jsp");
+		rd.forward(request, response);
+
 		/*
-		 * //teste de recebimento da lista
-		 *  for ( int i = 0; i < lista.size(); i++){
+		 * //teste de recebimento da lista for ( int i = 0; i < lista.size(); i++){
 		 * System.out.println(lista.get(i).getIdcon());
 		 * System.out.println(lista.get(i).getNome());
 		 * System.out.println(lista.get(i).getFone());
@@ -83,4 +91,44 @@ public class Controller extends HttpServlet {
 		response.sendRedirect("main");
 	}
 
+	// Editar contato
+	protected void listarContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Recebimento do id do contato que será editado
+			String idcon = request.getParameter("idcon");
+			
+		//Setando a variável Javabeans
+			contato.setIdcon(idcon);
+			
+		//Executar o método selecionarContato(DAO)
+			dao.selecionarContato(contato);
+			//teste de recebimento
+			System.out.println(contato.getIdcon());
+			System.out.println(contato.getNome());
+			System.out.println(contato.getFone());
+			System.out.println(contato.getEmail());
+		// Setar os atributos do formulário com o conteúdo JavaBeans
+			request.setAttribute("idcon", contato.getIdcon());
+			request.setAttribute("nome", contato.getNome());
+			request.setAttribute("fone", contato.getFone());
+			request.setAttribute("email", contato.getEmail());
+		
+		// Encaminhar ao documento editar.jsp
+			RequestDispatcher rd = request.getRequestDispatcher("editar.jsp");
+			rd.forward(request, response);
+	}
+	
+	protected void editarContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// setar as variaveis JavaBeans
+		contato.setIdcon(request.getParameter("idcon"));
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		
+		// Executar o método alterarContato
+		dao.alterarContato(contato);
+		
+		// Redirecionar para o documento agenda.jsp (atualizando as alterações)
+		response.sendRedirect("main");
+	}
 }
--- src/main/webapp/agenda.jsp
+++ src/main/webapp/agenda.jsp
@@ -1,5 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="utf-8"%>
+	pageEncoding="utf-8"%>
+<%@ page import="model.JavaBeans"%>
+<%@ page import="java.util.ArrayList"%>
+<%
+	ArrayList<JavaBeans> lista = (ArrayList<JavaBeans>)
+	request.getAttribute("contatos");
+	
+%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,6 +17,29 @@
 </head>
 <body>
 	<h1>Agenda de contatos</h1>
-	<a href = "novo.html" class="Botao1"> Novo contato</a>
+	<a href="novo.html" class="Botao1"> Novo contato</a>
+	
+	<table id = "tabela">
+	<thead>
+		<tr>
+			<th>Id</th>
+			<th>Nome</th>
+			<th>Fone</th>
+			<th>E-mail</th>
+			<th>Opções</th>
+		</tr>
+	</thead>
+	<tbody>
+		<%for (int i = 0; i< lista.size(); i++){ %>
+			<tr>
+				<td><%=lista.get(i).getIdcon()%></td>
+				<td><%=lista.get(i).getNome()%></td>
+				<td><%=lista.get(i).getFone()%></td>
+				<td><%=lista.get(i).getEmail()%></td>
+				<td> <a href="select?idcon=<%=lista.get(i).getIdcon() %>" class="Botao1">Editar</a></td>
+			</tr>
+		<%} %>
+	</tbody>
+	</table>
 </body>
 </html>
